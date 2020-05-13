35220ee1237ee4fe820fdd310f9cbc084ed7130d
Serge-Hand <61985138+Serge-Hand@users.noreply.github.com>
2020-05-13 17:20:01 +0300

    Взятие и приближение документов

---
+++ Assets/DoubleClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleClick : MonoBehaviour
+{
+    float doubleClickStart = 0; 
+    void OnMouseUp() 
+    { 
+        if ((Time.time - doubleClickStart) < 0.3f) 
+        {
+            Debug.Log("Double Clicked!");
+            GetComponent<Zoom>().DoubleClick();
+            doubleClickStart = -1; 
+        } 
+        else 
+        { 
+            doubleClickStart = Time.time; 
+        } 
+    }
+}
--- Assets/TurnCamera.cs
+++ Assets/TurnCamera.cs
@@ -13,17 +13,31 @@ public class TurnCamera : MonoBehaviour
             {
                 if (Input.GetKeyDown("space"))
                 {
-                    StartCoroutine(Turn());
+                    StartCoroutine(Turn("Turn"));
+                }
+                if (Input.GetKeyDown("f"))
+                {
+                    StartCoroutine(Turn("TurnPaper"));
                 }
             }
         }
     }
 
-    IEnumerator Turn()
+    IEnumerator Turn(string name)
     {
         isTurning = true;
-        GetComponent<Animator>().SetTrigger("Turn");
-        yield return new WaitForSeconds(1.0f);
+        GetComponent<Animator>().SetTrigger(name);
+        yield return new WaitForSeconds(0.01f);
+        if (name.Equals("Turn"))
+            if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("CamBoard") ||
+                GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("CamTable"))
+                yield return new WaitForSeconds(0.9f);
+        if (name.Equals("TurnPaper"))
+            if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("CamPaper") ||
+                GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("CamBack"))
+                yield return new WaitForSeconds(0.75f);
+        GetComponent<Animator>().ResetTrigger("Turn");
+        GetComponent<Animator>().ResetTrigger("TurnPaper");
         isTurning = false;
     }
 }
