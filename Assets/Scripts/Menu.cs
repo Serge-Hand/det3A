b16393fae00f17c44f0b4372ed578684931eb89f
Serge-Hand <61985138+Serge-Hand@users.noreply.github.com>
2020-05-28 00:08:21 +0300

    Menu added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("menuMusic");
+    }
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("intro");
+    }
+    public void QuitPressed()
+    {
+        Application.Quit();
+    }
+}
